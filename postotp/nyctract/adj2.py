d3af3bf2732fe35d3c11e5a5272e038eeb98153d
mayijun1203 <mayijun1203@gmail.com>
2020-01-03 15:32:43 -0500

    Update adj2.py

--- postotp/nyctract/adj2.py
+++ postotp/nyctract/adj2.py
@@ -369,8 +369,8 @@ def worktravelshed(arrt):
 
 # Define parallel multiprocessing function
 def parallelize(data, func):
-    data_split=np.array_split(data,np.ceil(len(data)/(mp.cpu_count()-7)))
-    pool=mp.Pool(mp.cpu_count()-7)
+    data_split=np.array_split(data,np.ceil(len(data)/(mp.cpu_count()-1)))
+    pool=mp.Pool(mp.cpu_count()-1)
     dt=pd.DataFrame()
     for i in data_split:
         ds=pd.concat(pool.map(func,i),axis=1)
