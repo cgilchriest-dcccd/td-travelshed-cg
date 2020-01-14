799176fbc41d5271dc383891c1f0c147b74315a0
mayijun1203 <mayijun1203@gmail.com>
2020-01-14 09:24:27 -0500

    Create eharlem.py

---
+++ postotp/eharlem/eharlem.py
@@ -0,0 +1,18 @@
+import datetime
+import time
+import geopandas as gpd
+import pandas as pd
+import numpy as np
+import shapely
+import requests
+
+start=datetime.datetime.now()
+
+pd.set_option('display.max_columns', None)
+path='/home/mayijun/TRAVELSHED/'
+#path='C:/Users/Yijun Ma/Desktop/D/DOCUMENT/DCP2018/TRAVELSHEDREVAMP/'
+#path='C:/Users/Y_Ma2/Desktop/amazon/'
+#path='C:/Users/Y_Ma2/Desktop/TEST/'
+#path='E:/TRAVELSHEDREVAMP/'
+
+
