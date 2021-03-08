197c5b3b09a0232c7aa6f815b8f418cf663ab608
Yijun Ma <mayijun1203@gmail.com>
2021-03-07 20:26:35 -0500

    update

--- runotp/otpmapboxbuild.sh
+++ runotp/otpmapboxbuild.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xmx10G -jar "/home/mayijun/TRAVELSHED/otp/otpmapbox/otp-1.5.0-shaded.jar" --build "/home/mayijun/TRAVELSHED/otp/otpmapbox" --analyst --port 8801 --securePort 8802
+java -Xmx10G -jar "/home/mayijun/TRAVELSHED/otp/otpmapbox/otp-1.4.0-shaded.jar" --build "/home/mayijun/TRAVELSHED/otp/otpmapbox" --analyst --port 8801 --securePort 8802
