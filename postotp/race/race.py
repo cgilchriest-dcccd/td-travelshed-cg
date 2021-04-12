0bc6069ca2f6fdccf8b43a56cb20fe7068193df5
Yijun Ma <mayijun1203@gmail.com>
2021-04-12 17:59:42 -0400

    update

--- postotp/race/race.py
+++ postotp/race/race.py
@@ -21,7 +21,18 @@ quadstatectclipped.crs=4326
 race=pd.merge(quadstatectclipped,race,how='inner',on='tractid')
 race=race[['tractid','TOTAL','WHITE','BLACK','NATIVE','ASIAN','PACIFIC','OTHER1','OTHER2','GRAVITYWAC','geometry']].reset_index(drop=True)
 race.to_file(path+'race/race.shp')
+race['JOBSTD']=(race['GRAVITYWAC']-np.mean(race['GRAVITYWAC']))/np.std(race['GRAVITYWAC'])
+race['JOBCAT']=np.where(race['JOBSTD']>=2,'>=+2SD',
+               np.where(race['JOBSTD']>=1.5,'+1.5SD ~ +2SD',
+               np.where(race['JOBSTD']>=1,'+1SD ~ +1.5SD',
+               np.where(race['JOBSTD']>=0.5,'+0.5SD ~ +1SD',
+               np.where(race['JOBSTD']>=0,'0SD ~ +0.5SD',
+               np.where(race['JOBSTD']>=-0.5,'-0.5SD ~ 0SD','<-0.5SD'))))))
+race['JOBPCT']=pd.qcut(race['GRAVITYWAC'],100,labels=False)+1
 race.to_file('C:/Users/mayij/Desktop/DOC/GITHUB/td-travelshed/postotp/race/race.geojson',driver='GeoJSON')
+
+
+
 
 
 
