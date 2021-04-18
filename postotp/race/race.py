3badd70ad8f92f5220eaea744d2ed294741be564
Yijun Ma <mayijun1203@gmail.com>
2021-04-18 17:15:41 -0400

    update

--- postotp/race/race.py
+++ postotp/race/race.py
@@ -93,6 +93,8 @@ df['ASIANA']=df['ASIANG']/df['ASIAN']
 df['PACIFICA']=df['PACIFICG']/df['PACIFIC']
 df['OTHER1A']=df['OTHER1G']/df['OTHER1']
 df['OTHER2A']=df['OTHER2G']/df['OTHER2']
+df['OTHERA']=(df['NATIVEG']+df['PACIFICG']+df['OTHER1G']+df['OTHER2G'])/(df['NATIVE']+df['PACIFIC']+df['OTHER1']+df['OTHER2'])
+
 df['TT']=1
 df=df.groupby(['TT'],as_index=False).agg({'TOTALG':'sum','TOTAL':'sum','WHITEG':'sum','WHITE':'sum',
                                           'BLACKG':'sum','BLACK':'sum','NATIVEG':'sum','NATIVE':'sum',
@@ -106,8 +108,81 @@ df['ASIANA']=df['ASIANG']/df['ASIAN']
 df['PACIFICA']=df['PACIFICG']/df['PACIFIC']
 df['OTHER1A']=df['OTHER1G']/df['OTHER1']
 df['OTHER2A']=df['OTHER2G']/df['OTHER2']
+df['OTHERA']=(df['NATIVEG']+df['PACIFICG']+df['OTHER1G']+df['OTHER2G'])/(df['NATIVE']+df['PACIFIC']+df['OTHER1']+df['OTHER2'])
 
 
+
+
+
+
+df=pd.read_csv(path+'nyctract/resbkgravity3.csv')
+df=df[['Unnamed: 0','GRAVITYWAC']].reset_index(drop=True)
+df.columns=['CT','GRAVITYWAC']
+inc=pd.read_csv(path+'race/income.csv')
+df=pd.merge(df,inc,how='inner',on='CT')
+df['COUNTY']=[x[3:8] for x in df['CT']]
+df['TOTALG']=df['TOTAL']*df['GRAVITYWAC']
+df['INC01G']=df['INC01']*df['GRAVITYWAC']
+df['INC02G']=df['INC02']*df['GRAVITYWAC']
+df['INC03G']=df['INC03']*df['GRAVITYWAC']
+df['INC04G']=df['INC04']*df['GRAVITYWAC']
+df['INC05G']=df['INC05']*df['GRAVITYWAC']
+df['INC06G']=df['INC06']*df['GRAVITYWAC']
+df['INC07G']=df['INC07']*df['GRAVITYWAC']
+df['INC08G']=df['INC08']*df['GRAVITYWAC']
+df['INC09G']=df['INC09']*df['GRAVITYWAC']
+df['INC10G']=df['INC10']*df['GRAVITYWAC']
+df['INC11G']=df['INC11']*df['GRAVITYWAC']
+df=df.groupby(['COUNTY'],as_index=False).agg({'TOTALG':'sum','TOTAL':'sum','INC01G':'sum','INC01':'sum',
+                                              'INC02G':'sum','INC02':'sum','INC03G':'sum','INC03':'sum',
+                                              'INC04G':'sum','INC04':'sum','INC05G':'sum','INC05':'sum',
+                                              'INC06G':'sum','INC06':'sum','INC07G':'sum','INC07':'sum',
+                                              'INC08G':'sum','INC08':'sum','INC09G':'sum','INC09':'sum',
+                                              'INC10G':'sum','INC10':'sum','INC11G':'sum','INC11':'sum'}).reset_index(drop=True)
+df['TOTALA']=df['TOTALG']/df['TOTAL']
+df['INC01A']=df['INC01G']/df['INC01']
+df['INC02A']=df['INC02G']/df['INC02']
+df['INC03A']=df['INC03G']/df['INC03']
+df['INC04A']=df['INC04G']/df['INC04']
+df['INC05A']=df['INC05G']/df['INC05']
+df['INC06A']=df['INC06G']/df['INC06']
+df['INC07A']=df['INC07G']/df['INC07']
+df['INC08A']=df['INC08G']/df['INC08']
+df['INC09A']=df['INC09G']/df['INC09']
+df['INC10A']=df['INC10G']/df['INC10']
+df['INC11A']=df['INC11G']/df['INC11']
+df['INC01CBNA']=(df['INC01G']+df['INC02G']+df['INC03G']+df['INC04G']+df['INC05G'])/(df['INC01']+df['INC02']+df['INC03']+df['INC04']+df['INC05'])
+df['INC02CBNA']=(df['INC06G']+df['INC07G'])/(df['INC06']+df['INC07'])
+df['INC03CBNA']=df['INC08G']/df['INC08']
+df['INC04CBNA']=df['INC09G']/df['INC09']
+df['INC05CBNA']=df['INC10G']/df['INC10']
+df['INC06CBNA']=df['INC11G']/df['INC11']
+
+df['TT']=1
+df=df.groupby(['TT'],as_index=False).agg({'TOTALG':'sum','TOTAL':'sum','INC01G':'sum','INC01':'sum',
+                                          'INC02G':'sum','INC02':'sum','INC03G':'sum','INC03':'sum',
+                                          'INC04G':'sum','INC04':'sum','INC05G':'sum','INC05':'sum',
+                                          'INC06G':'sum','INC06':'sum','INC07G':'sum','INC07':'sum',
+                                          'INC08G':'sum','INC08':'sum','INC09G':'sum','INC09':'sum',
+                                          'INC10G':'sum','INC10':'sum','INC11G':'sum','INC11':'sum'}).reset_index(drop=True)
+df['TOTALA']=df['TOTALG']/df['TOTAL']
+df['INC01A']=df['INC01G']/df['INC01']
+df['INC02A']=df['INC02G']/df['INC02']
+df['INC03A']=df['INC03G']/df['INC03']
+df['INC04A']=df['INC04G']/df['INC04']
+df['INC05A']=df['INC05G']/df['INC05']
+df['INC06A']=df['INC06G']/df['INC06']
+df['INC07A']=df['INC07G']/df['INC07']
+df['INC08A']=df['INC08G']/df['INC08']
+df['INC09A']=df['INC09G']/df['INC09']
+df['INC10A']=df['INC10G']/df['INC10']
+df['INC11A']=df['INC11G']/df['INC11']
+df['INC01CBNA']=(df['INC01G']+df['INC02G']+df['INC03G']+df['INC04G']+df['INC05G'])/(df['INC01']+df['INC02']+df['INC03']+df['INC04']+df['INC05'])
+df['INC02CBNA']=(df['INC06G']+df['INC07G'])/(df['INC06']+df['INC07'])
+df['INC03CBNA']=df['INC08G']/df['INC08']
+df['INC04CBNA']=df['INC09G']/df['INC09']
+df['INC05CBNA']=df['INC10G']/df['INC10']
+df['INC06CBNA']=df['INC11G']/df['INC11']
 
 
 
