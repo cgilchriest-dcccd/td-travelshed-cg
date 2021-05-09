70636b7664c28152909e5ec4d9711dcde546f8d5
Yijun Ma <mayijun1203@gmail.com>
2021-05-09 19:30:55 -0400

    update

---
+++ runotp/otpskimbuild.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -Xmx10G -jar "/home/mayijun/TRAVELSHED/otp/otpskim/otp-1.5.0-shaded.jar" --build "/home/mayijun/TRAVELSHED/otp/otpskim" --analyst --port 8801 --securePort 8802
---
+++ runotp/otpskimload.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -Xmx10G -jar "/home/mayijun/TRAVELSHED/otp/otpskim/otp-1.5.0-shaded.jar" --graphs "/home/mayijun/TRAVELSHED/otp/otpskim" --autoScan --analyst --port 8801 --securePort 8802
