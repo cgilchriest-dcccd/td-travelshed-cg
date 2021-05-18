6b1ad173fc6057367c07e6394492636cdf4d16d3
Yijun Ma <mayijun1203@gmail.com>
2021-05-18 18:56:40 -0400

    Update waterfrontwt2.py

--- postotp/waterfront/waterfrontwt2.py
+++ postotp/waterfront/waterfrontwt2.py
@@ -11,9 +11,14 @@ import numpy as np
 import shapely
 import requests
 import multiprocessing as mp
+import plotly.graph_objects as go
+import plotly.io as pio
+import json
+
 
 
 pd.set_option('display.max_columns', None)
+# pio.renderers.default = "browser"
 path='/home/mayijun/TRAVELSHED/'
 # path='C:/Users/mayij/Desktop/DOC/DCP2018/TRAVELSHEDREVAMP/'
 # doserver='http://159.65.64.166:8801/'
@@ -121,8 +126,8 @@ def travelshedwt(arrt):
         url+='&date='+typicaldate+'&time='+arrt+'&maxTransfers='+str(maxTransfers)
         url+='&maxWalkDistance='+str(maxWalkDistance)+'&clampInitialWait=0'+cutoff
     else:
-        print(destination.loc[i,'id']+' has no direction!')        
-    headers={'Accept':'application/json'}  
+        print(destination.loc[i,'id']+' has no direction!')
+    headers={'Accept':'application/json'}
     req=requests.get(url=url,headers=headers)
     js=req.json()
     iso=gpd.GeoDataFrame.from_features(js,crs=4326)
@@ -140,7 +145,7 @@ def travelshedwt(arrt):
                 if len(bk.loc[bk['T'+arrt[0:2]+arrt[3:5]]==cut[k]-cutoffinterval/2])!=0:
                     try:
                         bkiso=gpd.sjoin(bk.loc[bk['T'+arrt[0:2]+arrt[3:5]]==cut[k]-cutoffinterval/2],
-                                        iso.loc[iso['time']==cut[k+1]*60],how='inner',op='within')
+                                        iso.loc[iso['time']==cut[k+1]*60],how='inner',op='intersects')
                         bkiso=bkiso['blockid']
                         bk.loc[bk['blockid'].isin(bkiso),'T'+arrt[0:2]+arrt[3:5]]=cut[k+1]-cutoffinterval/2
                     except ValueError:
@@ -178,7 +183,7 @@ if __name__=='__main__':
     location['id']=['SITE'+str(x).zfill(4) for x in location['siteid']]
     location['latlong']=[str(x)+','+str(y) for x,y in zip(location['lat'],location['long'])]
     # destination=location.loc[0:max(location.count())-1,['id','parkid','name','direction','latlong']].reset_index(drop=True)
-    destination=location.loc[0:10,['id','parkid','name','direction','latlong']].reset_index(drop=True)
+    destination=location.loc[10:max(location.count())-1,['id','parkid','name','direction','latlong']].reset_index(drop=True)
     # Create travel time table for each site
     for i in destination.index:
         df=parallelize(arrivaltime,travelshedwt)
@@ -186,76 +191,142 @@ if __name__=='__main__':
         df=df['TTMEDIAN'].sort_index()
         df.name=destination.loc[i,'id']
         df.to_csv(path+'waterfront2/'+destination.loc[i,'id']+'wt.csv',index=True,header=True,na_rep=999)
-#    # Join site travelsheds to block shapefile
-#    wtbk=gpd.read_file(path+'shp/quadstatebkclipped.shp')
-#    wtbk.crs={'init': 'epsg:4326'}
-#    wtbk=wtbk[['blockid','geometry']]    
-#    for i in destination.index:
-#        tp=pd.read_csv(path+'waterfront/'+destination.loc[i,'id']+'wt.csv',dtype=str)
-#        tp.iloc[:,1]=pd.to_numeric(tp.iloc[:,1])
-#        wtbk=wtbk.merge(tp,on='blockid')
-#    wtbk.to_file(path+'waterfront/wtbk.shp')
-#    wtbk=wtbk.drop('geometry',axis=1)
-#    wtbk.to_csv(path+'waterfront/wtbk.csv',index=False)
-#    # Join site travelsheds to tract shapefile
-#    wtbk=wtbk.replace(999,np.nan)
-#    loclist=wtbk.columns[1:]
-#    wtbk['tractid']=[str(x)[0:11] for x in wtbk['blockid']]
-#    wtbk=wtbk.groupby(['tractid'])[loclist].median(skipna=True)
-#    wtbk=wtbk.replace(np.nan,999)
-#    wtbk=wtbk.reset_index()
-#    wtct=gpd.read_file(path+'shp/quadstatectclipped.shp')
-#    wtct.crs={'init': 'epsg:4326'}
-#    wtct=wtct[['tractid','geometry']]
-#    wtct=wtct.merge(wtbk,on='tractid')
-#    wtct.to_file(path+'waterfront/wtct.shp')
-#    wtct=wtct.drop('geometry',axis=1)
-#    wtct.to_csv(path+'waterfront/wtct.csv',index=False)
-#    # Create map for each site
-#    wtbk=gpd.read_file(path+'waterfront/wtbk.shp')
-#    wtbk.crs={'init': 'epsg:4326'}
-#    wtct=gpd.read_file(path+'waterfront/wtct.shp')
-#    wtct.crs={'init': 'epsg:4326'}
-#    for i in destination.index:
-#        # Create block level map
-#        wtbkmap=wtbk.loc[wtbk[destination.loc[i,'id']]<=90,[destination.loc[i,'id'],'geometry']]
-#        wtbkmap=wtbkmap.to_crs(epsg=3857)
-#        fig,ax=plt.subplots(1,figsize=(11,8.5))
-#        plt.subplots_adjust(left=0.05,right=0.95,top=0.95,bottom=0.05)
-#        ax=wtbkmap.plot(figsize=(11,8.5),edgecolor=None,column=destination.loc[i,'id'],cmap='Spectral',linewidth=0.2,ax=ax,alpha=0.7)
-#        add_basemap(ax,zoom=11,url=ctx.sources.ST_TONER_LITE)
-#        ax.set_axis_off()
-#        if destination.loc[i,'direction']=='in':
-#            ax.set_title('Saturday Midday Transit Travel Time (Minutes) to '+destination.loc[i,'id'],fontdict={'fontsize':'16','fontweight':'10'})
-#        elif destination.loc[i,'direction']=='out':
-#            ax.set_title('Saturday Midday Transit Travel Time (Minutes) from '+destination.loc[i,'id'],fontdict={'fontsize':'16','fontweight':'10'})
-#        sm=plt.cm.ScalarMappable(cmap='Spectral',norm=plt.Normalize(vmin=1,vmax=90))
-#        sm._A=[]
-#        divider=mpl_toolkits.axes_grid1.make_axes_locatable(ax)
-#        cax=divider.append_axes("right",size="3%",pad=0.2,aspect=25)
-#        cbar=fig.colorbar(sm,cax=cax)
-#        fig.tight_layout()
-#        fig.savefig(path+'waterfront/'+destination.loc[i,'id']+'wtbk.jpg', dpi=300)
-#        # Create tract level map
-#        wtctmap=wtct.loc[wtct[destination.loc[i,'id']]<=90,[destination.loc[i,'id'],'geometry']]
-#        wtctmap=wtctmap.to_crs(epsg=3857)
-#        fig,ax=plt.subplots(1,figsize=(11,8.5))
-#        plt.subplots_adjust(left=0.05,right=0.95,top=0.95,bottom=0.05)
-#        ax=wtctmap.plot(figsize=(11,8.5),edgecolor=None,column=destination.loc[i,'id'],cmap='Spectral',linewidth=0.2,ax=ax,alpha=0.7)
-#        add_basemap(ax,zoom=11,url=ctx.sources.ST_TONER_LITE)
-#        ax.set_axis_off()
-#        if destination.loc[i,'direction']=='in':
-#            ax.set_title('Saturday Midday Transit Travel Time (Minutes) to '+destination.loc[i,'id'],fontdict={'fontsize':'16','fontweight':'10'})
-#        elif destination.loc[i,'direction']=='out':
-#            ax.set_title('Saturday Midday Transit Travel Time (Minutes) from '+destination.loc[i,'id'],fontdict={'fontsize':'16','fontweight':'10'})
-#        sm=plt.cm.ScalarMappable(cmap='Spectral',norm=plt.Normalize(vmin=1,vmax=90))
-#        sm._A=[]
-#        divider=mpl_toolkits.axes_grid1.make_axes_locatable(ax)
-#        cax=divider.append_axes("right",size="3%",pad=0.2,aspect=25)
-#        cbar=fig.colorbar(sm,cax=cax)
-#        fig.tight_layout()
-#        fig.savefig(path+'waterfront/'+destination.loc[i,'id']+'wtct.jpg', dpi=300)
-#    print(datetime.datetime.now()-start)    
+    # # Join site travelsheds to block shapefile
+    # wtbk=gpd.read_file(path+'shp/quadstatebkclipped.shp')
+    # wtbk.crs=4326
+    # wtbk=wtbk.drop(['lat','long'],axis=1)
+    # for i in destination.index:
+    #     tp=pd.read_csv(path+'waterfront2/'+destination.loc[i,'id']+'wt.csv',dtype=float,converters={'blockid':str})
+    #     wtbk=wtbk.merge(tp,on='blockid')
+    # wtbk.to_file(path+'waterfront2/wtbk.shp')        
+    # wtbk=wtbk.drop('geometry',axis=1)
+    # wtbk.to_csv(path+'waterfront2/wtbk.csv',index=False)
+    # # Join site travelsheds to tract shapefile
+    # wtbk=wtbk.replace(999,np.nan)
+    # loclist=wtbk.columns[1:]
+    # wtbk['tractid']=[str(x)[0:11] for x in wtbk['blockid']]
+    # wtbk=wtbk.groupby(['tractid'],as_index=False)[loclist].median()
+    # wtbk=wtbk.replace(np.nan,999)
+    # wtct=gpd.read_file(path+'shp/quadstatectclipped.shp')
+    # wtct.crs=4326
+    # wtct=wtct.drop(['lat','long'],axis=1)
+    # wtct=wtct.merge(wtbk,on='tractid')
+    # wtct.to_file(path+'waterfront2/wtct.shp')
+    # wtct=wtct.drop('geometry',axis=1)
+    # wtct.to_csv(path+'waterfront2/wtct.csv',index=False)
+    # # Create map for each site
+    # wtbk=gpd.read_file(path+'waterfront2/wtbk.shp')
+    # wtbk.crs=4326
+    # wtct=gpd.read_file(path+'waterfront2/wtct.shp')
+    # wtct.crs=4326
+    # for i in destination.index:
+    #     # Create block level map
+    #     wtbkmap=wtbk.loc[wtbk[destination.loc[i,'id']]<=90,['blockid',destination.loc[i,'id'],'geometry']].reset_index(drop=True)
+    #     wtbkgjs=json.loads(wtbkmap.to_json())
+    #     p=go.Figure(go.Choroplethmapbox(geojson=wtbkgjs,
+    #                                     featureidkey='properties.blockid',
+    #                                     locations=wtbkmap['blockid'],
+    #                                     z=wtbkmap[destination.loc[i,'id']],
+    #                                     zmin=0,
+    #                                     zmax=90,
+    #                                     colorscale='RdBu',
+    #                                     reversescale=False,
+    #                                     colorbar={'title_text':'<b>Travel<br>Time<br>(mins)<br> <br></b>',
+    #                                               'title_font_size':16,
+    #                                               'x':0.99,
+    #                                               'xanchor':'right',
+    #                                               'xpad':20,
+    #                                               'y':0.99,
+    #                                               'yanchor':'top',
+    #                                               'ypad':20,
+    #                                               'len':0.5,
+    #                                               'outlinewidth':0,
+    #                                               'tickvals':list(range(0,100,10)),
+    #                                               'tickfont_size':12,
+    #                                               'bgcolor':'rgba(255,255,255,0.5)'},
+    #                                     marker={'line_color':'rgba(255,255,255,0)',
+    #                                             'line_width':0,
+    #                                             'opacity':0.8},
+    #                                 hovertext='<b>Census Tract: </b>'+
+    #                                           wtbkmap['blockid']+
+    #                                           '<br>'+
+    #                                           '<b>Transit Travel Time (mins): </b>'+
+    #                                           wtbkmap[destination.loc[i,'id']].astype(int).astype(str),
+    #                                 hoverinfo='text'))
+    #     p=p.add_trace(go.Scattermapbox(lat=[pd.to_numeric(destination.loc[i,'latlong'].split(',')[0])],
+    #                    lon=[pd.to_numeric(destination.loc[i,'latlong'].split(',')[-1])],
+    #                    mode='markers',
+    #                    marker={'size':5,
+    #                            'color':'black'},
+    #                    hoverinfo='none'))
+    #     p.update_layout(mapbox={'style':'carto-positron',
+    #                             'center':{'lat':np.mean([min(wtbkmap.bounds['miny']),max(wtbkmap.bounds['maxy'])]),
+    #                                     'lon':np.mean([min(wtbkmap.bounds['minx']),max(wtbkmap.bounds['maxx'])])},
+    #                             'zoom':8.5},
+    #                     title={'text':'<b>90-min Transit Travelshed from '+destination.loc[i,'id']+'</b>',
+    #                            'font_size':20},
+    #                     template='ggplot2',
+    #                     font={'family':'arial',
+    #                           'color':'black'},
+    #                     margin={'r':0,'t':40,'l':0,'b':0})
+    #     p.write_image(path+'waterfront2/'+destination.loc[i,'id']+'wtbk.jpeg',width=1600,height=900)
+    #     p.write_html(path+'waterfront2/'+destination.loc[i,'id']+'wtbk.html',
+    #                  include_plotlyjs='cdn',
+    #                  config={'displaylogo':False,'modeBarButtonsToRemove':['select2d','lasso2d']})
+    #     # Create tract level map
+    #     wtctmap=wtct.loc[wtct[destination.loc[i,'id']]<=90,['tractid',destination.loc[i,'id'],'geometry']].reset_index(drop=True)
+    #     wtctgjs=json.loads(wtctmap.to_json())
+    #     p=go.Figure(go.Choroplethmapbox(geojson=wtctgjs,
+    #                                     featureidkey='properties.tractid',
+    #                                     locations=wtctmap['tractid'],
+    #                                     z=wtctmap[destination.loc[i,'id']],
+    #                                     zmin=0,
+    #                                     zmax=90,
+    #                                     colorscale='RdBu',
+    #                                     reversescale=False,
+    #                                     colorbar={'title_text':'<b>Travel<br>Time<br>(mins)<br> <br></b>',
+    #                                               'title_font_size':16,
+    #                                               'x':0.99,
+    #                                               'xanchor':'right',
+    #                                               'xpad':20,
+    #                                               'y':0.99,
+    #                                               'yanchor':'top',
+    #                                               'ypad':20,
+    #                                               'len':0.5,
+    #                                               'outlinewidth':0,
+    #                                               'tickvals':list(range(0,100,10)),
+    #                                               'tickfont_size':12,
+    #                                               'bgcolor':'rgba(255,255,255,0.5)'},
+    #                                     marker={'line_color':'rgba(255,255,255,0)',
+    #                                             'line_width':0,
+    #                                             'opacity':0.8},
+    #                                 hovertext='<b>Census Tract: </b>'+
+    #                                           wtctmap['tractid']+
+    #                                           '<br>'+
+    #                                           '<b>Transit Travel Time (mins): </b>'+
+    #                                           wtctmap[destination.loc[i,'id']].astype(int).astype(str),
+    #                                 hoverinfo='text'))
+    #     p=p.add_trace(go.Scattermapbox(lat=[pd.to_numeric(destination.loc[i,'latlong'].split(',')[0])],
+    #                    lon=[pd.to_numeric(destination.loc[i,'latlong'].split(',')[-1])],
+    #                    mode='markers',
+    #                    marker={'size':5,
+    #                            'color':'black'},
+    #                    hoverinfo='none'))
+    #     p.update_layout(mapbox={'style':'carto-positron',
+    #                             'center':{'lat':np.mean([min(wtctmap.bounds['miny']),max(wtctmap.bounds['maxy'])]),
+    #                                     'lon':np.mean([min(wtctmap.bounds['minx']),max(wtctmap.bounds['maxx'])])},
+    #                             'zoom':8.5},
+    #                     title={'text':'<b>90-min Transit Travelshed from '+destination.loc[i,'id']+'</b>',
+    #                            'font_size':20},
+    #                     template='ggplot2',
+    #                     font={'family':'arial',
+    #                           'color':'black'},
+    #                     margin={'r':0,'t':40,'l':0,'b':0})
+    #     p.write_image(path+'waterfront2/'+destination.loc[i,'id']+'wtct.jpeg',width=1600,height=900)
+    #     p.write_html(path+'waterfront2/'+destination.loc[i,'id']+'wtct.html',
+    #                  include_plotlyjs='cdn',
+    #                  config={'displaylogo':False,'modeBarButtonsToRemove':['select2d','lasso2d']})
+    print(datetime.datetime.now()-start)    
 #    # Join park travelsheds to block shapefile
 #    wtbk=gpd.read_file(path+'shp/quadstatebkclipped.shp')
 #    wtbk.crs={'init': 'epsg:4326'}
