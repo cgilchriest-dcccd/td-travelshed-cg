c48c41e0dfc301fb6a3fbcbda37e1960358e3914
Yijun Ma <mayijun1203@gmail.com>
2021-12-01 18:03:56 -0500

    Update otpwtloadlong2.sh

--- runotp/otpwtloadlong2.sh
+++ runotp/otpwtloadlong2.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xmx32G -jar "/home/mayijun/TRAVELSHED/otp/otpwt2/otp-1.4.0-shaded.jar" --graphs "/home/mayijun/TRAVELSHED/otp/otpwt2" --autoScan --analyst --port 8801 --securePort 8802
+java -Xmx24G -jar "/home/mayijun/TRAVELSHED/otp/otpwt2/otp-1.4.0-shaded.jar" --graphs "/home/mayijun/TRAVELSHED/otp/otpwt2" --autoScan --analyst --port 8801 --securePort 8802
