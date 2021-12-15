a93715a2ba116e812cd942791c24f3588d7a592b
Yijun Ma <mayijun1203@gmail.com>
2021-12-15 14:49:57 -0500

    Update mobility2.py

--- postotp/mobility/mobility2.py
+++ postotp/mobility/mobility2.py
@@ -127,23 +127,32 @@ if __name__=='__main__':
     #     destination.to_csv(path+'mobility/isofrom.csv',index=False)
     #     print(i)
     
-    # destination=pd.read_csv(path+'mobility/isofrom.csv',dtype={'tractid':str})
-    # destination['std']=(destination['acre60']-np.mean(destination['acre60']))/np.std(destination['acre60'])
-    # destination['std'].hist(bins=100)
-    # destination['stdcat']=np.where(destination['std']>=2.5,'>=+2.5SD',
-    #                       np.where(destination['std']>=1.5,'+1.5SD ~ +2.5SD',
-    #                       np.where(destination['std']>=0.5,'+0.5SD ~ +1.5SD',
-    #                       np.where(destination['std']>=-0.5,'-0.5SD ~ +0.5SD',
-    #                       np.where(destination['std']>=-1.5,'-1.5SD ~ -0.5SD',
-    #                       np.where(destination['std']>=-2.5,'-2.5SD ~ -1.5SD','<-2.5SD'))))))
-    # destination['pct']=pd.qcut(destination['acre60'],100,labels=False)
-    # destination=destination[['tractid','acre60','std','stdcat','pct']].reset_index(drop=True)
-    # ct=gpd.read_file(path+'shp/quadstatectclipped.shp')
-    # ct.crs=4326
-    # ct=ct[['tractid','geometry']].reset_index(drop=True)
-    # destination=pd.merge(ct,destination,how='inner',on='tractid')
-    # destination.to_file(path+'mobility/isofrom.shp')
-    # print(datetime.datetime.now()-start)
+    destination=pd.read_csv(path+'mobility/isofrom.csv',dtype={'tractid':str})
+    destination['std']=(destination['acre60']-np.mean(destination['acre60']))/np.std(destination['acre60'])
+    destination['std'].hist(bins=100)
+    destination['stdcat']=np.where(destination['std']>=2.5,'>=+2.5SD',
+                          np.where(destination['std']>=1.5,'+1.5SD ~ +2.5SD',
+                          np.where(destination['std']>=0.5,'+0.5SD ~ +1.5SD',
+                          np.where(destination['std']>=-0.5,'-0.5SD ~ +0.5SD',
+                          np.where(destination['std']>=-1.5,'-1.5SD ~ -0.5SD',
+                          np.where(destination['std']>=-2.5,'-2.5SD ~ -1.5SD','<-2.5SD'))))))
+    destination['pct']=pd.qcut(destination['acre60'],100,labels=False)
+    destination['acre60'].hist(bins=100)
+    m=destination.loc[destination['acre60']<=100000,'acre60'].mean()
+    s=destination.loc[destination['acre60']<=100000,'acre60'].std()
+    destination['score']=np.where(destination['acre60']>=m+1.5*s,'Very High', 
+                         np.where(destination['acre60']>=m+0.5*s,'High',
+                         np.where(destination['acre60']>=m-0.5*s,'Medium',
+                         np.where(destination['acre60']>=m-1.5*s,'Low','Very Low'))))
+    destination['score'].hist()
+    destination['score'].value_counts()
+    destination=destination[['tractid','acre60','std','stdcat','pct','score']].reset_index(drop=True)
+    ct=gpd.read_file(path+'shp/quadstatectclipped.shp')
+    ct.crs=4326
+    ct=ct[['tractid','geometry']].reset_index(drop=True)
+    destination=pd.merge(ct,destination,how='inner',on='tractid')
+    destination.to_file(path+'mobility/isofrom.shp')
+    print(datetime.datetime.now()-start)
     
     # location=pd.read_excel(path+'nyctract/centroid/centroid.xlsx',sheet_name='nycworktractptadjfinal',dtype=str)
     # location['tractid']=location['censustract'].copy()
@@ -162,23 +171,32 @@ if __name__=='__main__':
     #     destination.to_csv(path+'mobility/isoto.csv',index=False)
     #     print(i)
         
-    # destination=pd.read_csv(path+'mobility/isoto.csv',dtype={'tractid':str})
-    # destination['std']=(destination['acre60']-np.mean(destination['acre60']))/np.std(destination['acre60'])
-    # destination['std'].hist(bins=100)
-    # destination['stdcat']=np.where(destination['std']>=2.5,'>=+2.5SD',
-    #                       np.where(destination['std']>=1.5,'+1.5SD ~ +2.5SD',
-    #                       np.where(destination['std']>=0.5,'+0.5SD ~ +1.5SD',
-    #                       np.where(destination['std']>=-0.5,'-0.5SD ~ +0.5SD',
-    #                       np.where(destination['std']>=-1.5,'-1.5SD ~ -0.5SD',
-    #                       np.where(destination['std']>=-2.5,'-2.5SD ~ -1.5SD','<-2.5SD'))))))
-    # destination['pct']=pd.qcut(destination['acre60'],100,labels=False)
-    # destination=destination[['tractid','acre60','std','stdcat','pct']].reset_index(drop=True)
-    # ct=gpd.read_file(path+'shp/quadstatectclipped.shp')
-    # ct.crs=4326
-    # ct=ct[['tractid','geometry']].reset_index(drop=True)
-    # destination=pd.merge(ct,destination,how='inner',on='tractid')
-    # destination.to_file(path+'mobility/isoto.shp')
-    # print(datetime.datetime.now()-start)
+    destination=pd.read_csv(path+'mobility/isoto.csv',dtype={'tractid':str})
+    destination['std']=(destination['acre60']-np.mean(destination['acre60']))/np.std(destination['acre60'])
+    destination['std'].hist(bins=100)
+    destination['stdcat']=np.where(destination['std']>=2.5,'>=+2.5SD',
+                          np.where(destination['std']>=1.5,'+1.5SD ~ +2.5SD',
+                          np.where(destination['std']>=0.5,'+0.5SD ~ +1.5SD',
+                          np.where(destination['std']>=-0.5,'-0.5SD ~ +0.5SD',
+                          np.where(destination['std']>=-1.5,'-1.5SD ~ -0.5SD',
+                          np.where(destination['std']>=-2.5,'-2.5SD ~ -1.5SD','<-2.5SD'))))))
+    destination['pct']=pd.qcut(destination['acre60'],100,labels=False)
+    destination['acre60'].hist(bins=100)
+    m=destination.loc[destination['acre60']<=100000,'acre60'].mean()
+    s=destination.loc[destination['acre60']<=100000,'acre60'].std()
+    destination['score']=np.where(destination['acre60']>=m+1.5*s,'Very High', 
+                         np.where(destination['acre60']>=m+0.5*s,'High',
+                         np.where(destination['acre60']>=m-0.5*s,'Medium',
+                         np.where(destination['acre60']>=m-1.5*s,'Low','Very Low'))))
+    destination['score'].hist()
+    destination['score'].value_counts()
+    destination=destination[['tractid','acre60','std','stdcat','pct','score']].reset_index(drop=True)
+    ct=gpd.read_file(path+'shp/quadstatectclipped.shp')
+    ct.crs=4326
+    ct=ct[['tractid','geometry']].reset_index(drop=True)
+    destination=pd.merge(ct,destination,how='inner',on='tractid')
+    destination.to_file(path+'mobility/isoto.shp')
+    print(datetime.datetime.now()-start)
     
     # Mobility Example
     df=pd.read_csv(path+'mobility/isofrom.csv',dtype={'tractid':str})
