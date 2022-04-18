e2f8773974d0ea8c73f5c96c207ca4e9cfd48d2a
Yijun Ma <mayijun1203@gmail.com>
2022-04-18 17:26:32 -0400

    ibx

---
+++ runotp/otpibxload.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -Xmx10G -jar "/home/mayijun/TRAVELSHED/otp/otpibx/otp-1.5.0-shaded.jar" --graphs "/home/mayijun/TRAVELSHED/otp/otpibx" --autoScan --analyst --port 8801 --securePort 8802
